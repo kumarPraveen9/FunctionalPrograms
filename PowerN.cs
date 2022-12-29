d1df66ec4fcf2a34b747b7846a92525b213daed2
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-29 23:34:26 +0530

    Calculating N power of 2

---
+++ PowerN.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class PowerN
+    {
+        
+        
+            public static void pow()
+            {
+                Console.WriteLine("***************nth Power***************");
+                Console.WriteLine("Hey Buddy, please enter a value: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if ((n >= 0) && (n < 31))
+                {
+                    for (int i = 0; i <= n; i++)
+                    {
+                        double result = Math.Pow(2, i);
+                        Console.WriteLine(result);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("enter valid n value");
+                }
+
+            }
+        
+    }
+
+}
+
