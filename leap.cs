098ffcef7091fd3826a049dd3b232764a2ffe5bf
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-29 23:33:35 +0530

    Leap year or not !!

---
+++ leap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class leap
+    {
+       
+            public static void display()
+            {
+            Console.WriteLine("**************leap year or not****************");
+            Console.WriteLine("Enter a Year to check whether it is leap year or not");
+                int year = Convert.ToInt32(Console.ReadLine());
+                if ((year > 999) && (year < 10000))
+                {
+                    if (year % 400 == 0)
+                    {
+                        Console.WriteLine("Yeah, your year is leap year");
+                    }
+                    else if ((year % 4 == 0) && (year % 100 != 0))
+                    {
+                        Console.WriteLine("Yeah, your year is leap year");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Oops, your year is not leap year");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("enter a valid year");
+                }
+            }
+        }
+    }
+
